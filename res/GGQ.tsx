da88c2de7620725d647df56f05bae4732f5f1579
Shamyl Zakariya <shamyl@gmail.com>
2020-11-04 09:06:53 -0800

    Add spritesheet and level 1 tilemap

---
+++ res/GGQ.tsx
@@ -0,0 +1,181 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="GGQ" tilewidth="16" tileheight="16" spacing="1" tilecount="64" columns="8">
+ <image source="GGQ_l1_sprites.png" width="135" height="135"/>
+ <tile id="0">
+  <properties>
+   <property name="collision_shape" value="square"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="collision_shape" value="square"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="collision_shape" value="square"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="collision_shape" value="square"/>
+   <property name="viscous" value="true"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="collision_shape" value="triangle_nw"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="collision_shape" value="triangle_ne"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="collision_shape" value="square"/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="collision_shape" value="square"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="collision_shape" value="square"/>
+  </properties>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="collision_shape" value="square"/>
+  </properties>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="collision_shape" value="square"/>
+  </properties>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="collision_shape" value="square"/>
+  </properties>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="collision_shape" value="square"/>
+  </properties>
+ </tile>
+ <tile id="13">
+  <properties>
+   <property name="collision_shape" value="square"/>
+  </properties>
+ </tile>
+ <tile id="14">
+  <properties>
+   <property name="collision_shape" value="none"/>
+  </properties>
+ </tile>
+ <tile id="15">
+  <properties>
+   <property name="collision_shape" value="square"/>
+   <property name="falls" value="true"/>
+  </properties>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="collision_shape" value="none"/>
+  </properties>
+ </tile>
+ <tile id="17">
+  <properties>
+   <property name="collision_shape" value="none"/>
+  </properties>
+ </tile>
+ <tile id="18">
+  <properties>
+   <property name="collision_shape" value="none"/>
+  </properties>
+ </tile>
+ <tile id="19">
+  <properties>
+   <property name="collision_shape" value="none"/>
+  </properties>
+ </tile>
+ <tile id="20">
+  <properties>
+   <property name="collision_shape" value="none"/>
+  </properties>
+ </tile>
+ <tile id="21">
+  <properties>
+   <property name="collision_shape" value="none"/>
+  </properties>
+ </tile>
+ <tile id="22">
+  <properties>
+   <property name="collision_shape" value="none"/>
+  </properties>
+ </tile>
+ <tile id="23">
+  <properties>
+   <property name="collision_shape" value="none"/>
+  </properties>
+ </tile>
+ <tile id="24">
+  <properties>
+   <property name="collision_shape" value="none"/>
+  </properties>
+ </tile>
+ <tile id="25">
+  <properties>
+   <property name="collision_shape" value="none"/>
+  </properties>
+ </tile>
+ <tile id="26">
+  <properties>
+   <property name="collision_shape" value="none"/>
+  </properties>
+ </tile>
+ <tile id="27">
+  <properties>
+   <property name="collision_shape" value="none"/>
+  </properties>
+ </tile>
+ <tile id="28">
+  <properties>
+   <property name="collision_shape" value="none"/>
+  </properties>
+ </tile>
+ <tile id="29">
+  <properties>
+   <property name="collision_shape" value="none"/>
+  </properties>
+ </tile>
+ <tile id="30">
+  <properties>
+   <property name="collision_shape" value="none"/>
+  </properties>
+ </tile>
+ <tile id="31">
+  <properties>
+   <property name="collision_shape" value="none"/>
+  </properties>
+ </tile>
+ <tile id="32">
+  <properties>
+   <property name="collision_shape" value="none"/>
+  </properties>
+ </tile>
+ <tile id="33">
+  <properties>
+   <property name="collision_shape" value="none"/>
+  </properties>
+ </tile>
+ <tile id="34">
+  <properties>
+   <property name="collision_shape" value="none"/>
+  </properties>
+ </tile>
+</tileset>
