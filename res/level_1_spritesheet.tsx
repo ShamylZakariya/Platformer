ca8e3c3c05d08148c8343e0d86d931cbbec6583c
Shamyl Zakariya <shamyl@gmail.com>
2020-11-04 17:13:01 -0800

    Renamed level and spritesheet files, and added Tiled project.

--- res/level_1_spritesheet.tsx
+++ res/level_1_spritesheet.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.2" name="GGQ" tilewidth="16" tileheight="16" spacing="1" tilecount="64" columns="8">
- <image source="GGQ_l1_sprites.png" width="135" height="135"/>
+ <image source="level_1_spritesheet.png" width="135" height="135"/>
  <tile id="0">
   <properties>
    <property name="collision_shape" value="square"/>
