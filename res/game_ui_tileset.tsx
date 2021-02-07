d7c42da4860161df9eeae6931e149d3f784ca74c
Shamyl Zakariya <shamyl@gmail.com>
2021-02-07 14:31:29 -0800

    Added basic tileset/map info for in-game UI

---
+++ res/game_ui_tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="game_ui_tileset" tilewidth="8" tileheight="8" spacing="1" tilecount="225" columns="15">
+ <image source="game_ui_tileset.png" width="135" height="135"/>
+</tileset>
