6f1a2c97967b27f59bbfcf352aff254a62587d1d
Shamyl Zakariya <shamyl@gmail.com>
2021-02-10 08:16:56 -0800

    Flesh out the game ui tileset
    - adds health dots
    - adds text characters, except Q,X,Z which I coulndnt' find yet in game

--- res/game_ui_tileset.tsx
+++ res/game_ui_tileset.tsx
@@ -1,4 +1,159 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.3" name="game_ui_tileset" tilewidth="8" tileheight="8" spacing="1" tilecount="225" columns="15">
  <image source="game_ui_tileset.png" width="135" height="135"/>
+ <tile id="4">
+  <properties>
+   <property name="name" value="health_dot_full"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="name" value="health_dot_empty"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="name" value="character_a"/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="name" value="character_b"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="name" value="character_c"/>
+  </properties>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="name" value="character_d"/>
+  </properties>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="name" value="character_e"/>
+  </properties>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="name" value="character_f"/>
+  </properties>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="name" value="character_g"/>
+  </properties>
+ </tile>
+ <tile id="13">
+  <properties>
+   <property name="name" value="character_h"/>
+  </properties>
+ </tile>
+ <tile id="14">
+  <properties>
+   <property name="name" value="character_i"/>
+  </properties>
+ </tile>
+ <tile id="15">
+  <properties>
+   <property name="name" value="character_j"/>
+  </properties>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="name" value="character_k"/>
+  </properties>
+ </tile>
+ <tile id="17">
+  <properties>
+   <property name="name" value="character_l"/>
+  </properties>
+ </tile>
+ <tile id="18">
+  <properties>
+   <property name="name" value="character_m"/>
+  </properties>
+ </tile>
+ <tile id="19">
+  <properties>
+   <property name="name" value="character_n"/>
+  </properties>
+ </tile>
+ <tile id="20">
+  <properties>
+   <property name="name" value="character_o"/>
+  </properties>
+ </tile>
+ <tile id="21">
+  <properties>
+   <property name="name" value="character_p"/>
+  </properties>
+ </tile>
+ <tile id="22">
+  <properties>
+   <property name="name" value="character_q"/>
+  </properties>
+ </tile>
+ <tile id="23">
+  <properties>
+   <property name="name" value="character_r"/>
+  </properties>
+ </tile>
+ <tile id="24">
+  <properties>
+   <property name="name" value="character_s"/>
+  </properties>
+ </tile>
+ <tile id="25">
+  <properties>
+   <property name="name" value="character_t"/>
+  </properties>
+ </tile>
+ <tile id="26">
+  <properties>
+   <property name="name" value="character_u"/>
+  </properties>
+ </tile>
+ <tile id="27">
+  <properties>
+   <property name="name" value="character_v"/>
+  </properties>
+ </tile>
+ <tile id="28">
+  <properties>
+   <property name="name" value="character_w"/>
+  </properties>
+ </tile>
+ <tile id="29">
+  <properties>
+   <property name="name" value="character_x"/>
+  </properties>
+ </tile>
+ <tile id="30">
+  <properties>
+   <property name="name" value="character_y"/>
+  </properties>
+ </tile>
+ <tile id="31">
+  <properties>
+   <property name="name" value="character_z"/>
+  </properties>
+ </tile>
+ <tile id="32">
+  <properties>
+   <property name="name" value="character_period"/>
+  </properties>
+ </tile>
+ <tile id="33">
+  <properties>
+   <property name="name" value="character_dash"/>
+  </properties>
+ </tile>
+ <tile id="34">
+  <properties>
+   <property name="name" value="character_exclamation_point"/>
+  </properties>
+ </tile>
 </tileset>
