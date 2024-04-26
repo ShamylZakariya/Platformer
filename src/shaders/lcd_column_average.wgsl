f7de49b77c73feceb15ca86b11344b64c0f191fb
Shamyl Zakariya <shamyl@gmail.com>
2024-04-26 08:40:24 -0700

    Checkpoint - create column averaging pipeline and get the shader to compile.
    It's not being executed, yet.

--- src/shaders/lcd_column_average.wgsl
+++ src/shaders/lcd_column_average.wgsl
@@ -20,9 +20,6 @@ struct LcdUniforms {
 var color_attachment_texture: texture_2d_array<f32>;
 
 @group(0) @binding(1)
-var tonemap_texture: texture_2d<f32>;
-
-@group(0) @binding(2)
 var color_sampler: sampler;
 
 @group(1) @binding(0)
@@ -31,7 +28,7 @@ var<uniform> lcd_uniforms: LcdUniforms;
 ///////////////////////////////////////////////////////////////////////////////
 
 @vertex
-fn lcd_column_average_vs_main(@builtin(vertex_index) in_vertex_index: u32) -> FragmentInput {
+fn vs_main(@builtin(vertex_index) in_vertex_index: u32) -> FragmentInput {
     // wgsl doesn't let us index `let` arrays with a variable. So it has to be a `var` local to this function.
     var fsq_clip_positions: array<vec4<f32>,3> = array<vec4<f32>, 3>(vec4<f32>(-1.0, 1.0, 0.0, 1.0), vec4<f32>(3.0, 1.0, 0.0, 1.0), vec4<f32>(-1.0, -3.0, 0.0, 1.0));
     var fsq_tex_coords: array<vec2<f32>,3> = array<vec2<f32>, 3>(vec2<f32>(0.0, 0.0), vec2<f32>(2.0, 0.0), vec2<f32>(0.0, 2.0));
@@ -44,14 +41,14 @@ fn lcd_column_average_vs_main(@builtin(vertex_index) in_vertex_index: u32) -> Fr
 }
 
 fn average_column(tc: vec2<f32>) -> f32 {
-    let rows:u32 = color_attachment_size.y;
+    let rows:u32 = lcd_uniforms.color_attachment_size.y;
     let row_step: f32 = 1.0 / f32(rows);
     let layer = lcd_uniforms.color_attachment_layer_index;
 
     var accumulator:f32 = 0.0;
     var row_tc = 0.0;
 
-    for (var i:u32 = 0; i < rows; i += 1_u32) {
+    for (var i = 0u; i < rows; i += 1u) {
         let tex_coord = vec2<f32>(tc.x, row_tc);
         accumulator += textureSample(color_attachment_texture, color_sampler, tex_coord, layer).r;
         row_tc += row_step;
@@ -61,7 +58,7 @@ fn average_column(tc: vec2<f32>) -> f32 {
 }
 
 @fragment
-fn lcd_column_average_fs_main_main(in: FragmentInput) -> @location(0) vec4<f32> {
-    let v = average_column(in.tc);
+fn fs_main(in: FragmentInput) -> @location(0) vec4<f32> {
+    let v = average_column(in.tex_coord);
     return vec4<f32>(v, v, v, 1.0);
 }
--- src/state/lcd_filter.rs
+++ src/state/lcd_filter.rs
@@ -99,6 +99,10 @@ pub struct LcdFilter {
     display_pass_textures_bind_group_layout: wgpu::BindGroupLayout,
     display_pass_textures_bind_group: wgpu::BindGroup,
 
+    column_avg_pass_pipeline: wgpu::RenderPipeline,
+    column_avg_pass_textures_bind_group_layout: wgpu::BindGroupLayout,
+    column_avg_pass_textures_bind_group: wgpu::BindGroup,
+
     uniforms: LcdUniforms,
     tonemap: Texture,
 
@@ -115,14 +119,22 @@ impl LcdFilter {
 
         let display_pass = Self::create_display_pass(gpu, &uniforms, gpu.config.format, &tonemap);
 
+        let column_averaging_pass =
+            Self::create_column_averaging_pass(gpu, &uniforms, gpu.config.format);
+
         let lcd_hysteresis = (!options.no_hysteresis).then_some(Self::DEFAULT_HYSTERESIS);
 
         Self {
             display_pass_pipeline: display_pass.0,
             display_pass_textures_bind_group_layout: display_pass.1,
             display_pass_textures_bind_group: display_pass.2,
-            tonemap,
+
+            column_avg_pass_pipeline: column_averaging_pass.0,
+            column_avg_pass_textures_bind_group_layout: column_averaging_pass.1,
+            column_avg_pass_textures_bind_group: column_averaging_pass.2,
+
             uniforms,
+            tonemap,
             lcd_hysteresis,
             frames_available_for_hysteresis: 0,
         }
@@ -135,6 +147,110 @@ impl LcdFilter {
     pub fn lcd_hysteresis(&self) -> Option<std::time::Duration> {
         self.lcd_hysteresis
     }
+
+    pub fn resize(
+        &mut self,
+        _window: &Window,
+        _new_size: winit::dpi::PhysicalSize<u32>,
+        gpu: &gpu_state::GpuState,
+    ) {
+        // new color buffer means we lose our history sample range
+        self.frames_available_for_hysteresis = 0;
+
+        self.display_pass_textures_bind_group = Self::create_display_pass_textures_bind_group(
+            gpu,
+            &self.display_pass_textures_bind_group_layout,
+            &self.tonemap.view,
+        );
+
+        self.column_avg_pass_textures_bind_group =
+            Self::create_column_averaging_pass_textures_bind_group(
+                gpu,
+                &self.column_avg_pass_textures_bind_group_layout,
+            );
+    }
+
+    pub fn update(&mut self, ctx: &mut AppContext, game: &game_state::GameState) {
+        // Determine an appropriate alpha for pixel effects - as window gets
+        // smaller the effect needs to fade out, since it looks busy on small windows.
+        // NOTE: min_high_freq and max_high_freq were determined via experimentation
+        let pixel_effect_alpha = {
+            let frequency = (game.camera_controller.projection.scale() * game.pixels_per_unit.x)
+                / ctx.gpu.config.width as f32;
+
+            let min_high_freq = 0.2;
+            let max_high_freq = 0.5;
+            let falloff =
+                ((frequency - min_high_freq) / (max_high_freq - min_high_freq)).clamp(0.0, 1.0);
+            1.0 - (falloff * falloff)
+        };
+
+        let color_attachment_extent = ctx.gpu.color_attachment.extent;
+        let layer_count = color_attachment_extent.depth_or_array_layers;
+        let current_layer = ctx.frame_idx % layer_count;
+        let history_count = self
+            .lcd_hysteresis
+            .map_or_else(
+                || 1,
+                |hysteresis| {
+                    (hysteresis.as_secs_f32() / ctx.real_delta_time.as_secs_f32()).ceil() as u32
+                },
+            )
+            .min(self.frames_available_for_hysteresis as u32)
+            .max(1_u32);
+
+        self.uniforms
+            .data
+            .set_pixel_effect_alpha(pixel_effect_alpha)
+            .set_camera_position(game.camera_controller.camera.position().xy())
+            .set_pixels_per_unit(game.pixels_per_unit)
+            .set_viewport_size(game.camera_controller.projection.viewport_size())
+            .set_color_attachment_layer_index(current_layer)
+            .set_color_attachment_extent(color_attachment_extent)
+            .set_color_attachment_history_count(history_count);
+
+        self.uniforms.write(&mut ctx.gpu.queue);
+    }
+
+    pub fn render(
+        &mut self,
+        _window: &Window,
+        gpu: &mut gpu_state::GpuState,
+        output: &wgpu::SurfaceTexture,
+        encoder: &mut wgpu::CommandEncoder,
+        _frame_index: usize,
+    ) {
+        let view = output
+            .texture
+            .create_view(&wgpu::TextureViewDescriptor::default());
+
+        let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+            label: Some("LcdFilter Render Pass"),
+            color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                view: &view,
+                resolve_target: None,
+                ops: wgpu::Operations {
+                    load: wgpu::LoadOp::Load,
+                    store: wgpu::StoreOp::Store,
+                },
+            })],
+            depth_stencil_attachment: None,
+            timestamp_writes: None,
+            occlusion_query_set: None,
+        });
+
+        render_pass.set_pipeline(&self.display_pass_pipeline);
+        render_pass.set_bind_group(0, &self.display_pass_textures_bind_group, &[]);
+        render_pass.set_bind_group(1, &self.uniforms.bind_group, &[]);
+        render_pass.draw(0..3, 0..1); //FSQ
+
+        self.frames_available_for_hysteresis = (self.frames_available_for_hysteresis + 1)
+            .min(gpu.color_attachment.layer_array_views.len());
+    }
+
+    //
+    //  Display Pass
+    //
 
     fn create_display_pass_textures_bind_group(
         gpu: &gpu_state::GpuState,
@@ -270,96 +386,123 @@ impl LcdFilter {
         (pipeline, textures_bind_group_layout, textures_bind_group)
     }
 
-    pub fn resize(
-        &mut self,
-        _window: &Window,
-        _new_size: winit::dpi::PhysicalSize<u32>,
+    //
+    //  Column Averaging Pass
+    //
+
+    fn create_column_averaging_pass_textures_bind_group(
         gpu: &gpu_state::GpuState,
-    ) {
-        // new color buffer means we lose our history sample range
-        self.frames_available_for_hysteresis = 0;
-        self.display_pass_textures_bind_group = Self::create_display_pass_textures_bind_group(
-            gpu,
-            &self.display_pass_textures_bind_group_layout,
-            &self.tonemap.view,
-        );
-    }
-
-    pub fn update(&mut self, ctx: &mut AppContext, game: &game_state::GameState) {
-        // Determine an appropriate alpha for pixel effects - as window gets
-        // smaller the effect needs to fade out, since it looks busy on small windows.
-        // NOTE: min_high_freq and max_high_freq were determined via experimentation
-        let pixel_effect_alpha = {
-            let frequency = (game.camera_controller.projection.scale() * game.pixels_per_unit.x)
-                / ctx.gpu.config.width as f32;
-
-            let min_high_freq = 0.2;
-            let max_high_freq = 0.5;
-            let falloff =
-                ((frequency - min_high_freq) / (max_high_freq - min_high_freq)).clamp(0.0, 1.0);
-            1.0 - (falloff * falloff)
-        };
-
-        let color_attachment_extent = ctx.gpu.color_attachment.extent;
-        let layer_count = color_attachment_extent.depth_or_array_layers;
-        let current_layer = ctx.frame_idx % layer_count;
-        let history_count = self
-            .lcd_hysteresis
-            .map_or_else(
-                || 1,
-                |hysteresis| {
-                    (hysteresis.as_secs_f32() / ctx.real_delta_time.as_secs_f32()).ceil() as u32
+        layout: &wgpu::BindGroupLayout,
+    ) -> wgpu::BindGroup {
+        gpu.device.create_bind_group(&wgpu::BindGroupDescriptor {
+            layout,
+            entries: &[
+                wgpu::BindGroupEntry {
+                    binding: 0,
+                    resource: wgpu::BindingResource::TextureView(&gpu.color_attachment.view),
                 },
-            )
-            .min(self.frames_available_for_hysteresis as u32)
-            .max(1_u32);
-
-        self.uniforms
-            .data
-            .set_pixel_effect_alpha(pixel_effect_alpha)
-            .set_camera_position(game.camera_controller.camera.position().xy())
-            .set_pixels_per_unit(game.pixels_per_unit)
-            .set_viewport_size(game.camera_controller.projection.viewport_size())
-            .set_color_attachment_layer_index(current_layer)
-            .set_color_attachment_extent(color_attachment_extent)
-            .set_color_attachment_history_count(history_count);
-
-        self.uniforms.write(&mut ctx.gpu.queue);
+                wgpu::BindGroupEntry {
+                    binding: 1,
+                    resource: wgpu::BindingResource::Sampler(&gpu.color_attachment.sampler),
+                },
+            ],
+            label: Some("LcdFilter Column Averaging Pass Bind Group"),
+        })
     }
 
-    pub fn render(
-        &mut self,
-        _window: &Window,
+    fn create_column_averaging_pass(
         gpu: &mut gpu_state::GpuState,
-        output: &wgpu::SurfaceTexture,
-        encoder: &mut wgpu::CommandEncoder,
-        _frame_index: usize,
-    ) {
-        let view = output
-            .texture
-            .create_view(&wgpu::TextureViewDescriptor::default());
+        lcd_uniforms: &LcdUniforms,
+        color_format: wgpu::TextureFormat,
+    ) -> (wgpu::RenderPipeline, wgpu::BindGroupLayout, wgpu::BindGroup) {
+        let textures_bind_group_layout =
+            gpu.device
+                .create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+                    label: Some("LcdFilter Column Averaging Pass Bind Group Layout"),
+                    entries: &[
+                        // Color attachment
+                        wgpu::BindGroupLayoutEntry {
+                            binding: 0,
+                            visibility: wgpu::ShaderStages::FRAGMENT,
+                            ty: wgpu::BindingType::Texture {
+                                multisampled: false,
+                                sample_type: wgpu::TextureSampleType::Float { filterable: false },
+                                view_dimension: wgpu::TextureViewDimension::D2Array,
+                            },
+                            count: None,
+                        },
+                        // Sampler
+                        wgpu::BindGroupLayoutEntry {
+                            binding: 1,
+                            visibility: wgpu::ShaderStages::FRAGMENT,
+                            ty: wgpu::BindingType::Sampler(wgpu::SamplerBindingType::NonFiltering),
+                            count: None,
+                        },
+                    ],
+                });
 
-        let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
-            label: Some("LcdFilter Render Pass"),
-            color_attachments: &[Some(wgpu::RenderPassColorAttachment {
-                view: &view,
-                resolve_target: None,
-                ops: wgpu::Operations {
-                    load: wgpu::LoadOp::Load,
-                    store: wgpu::StoreOp::Store,
+        let textures_bind_group = Self::create_column_averaging_pass_textures_bind_group(
+            gpu,
+            &textures_bind_group_layout,
+        );
+
+        let lcd_wgsl = wgpu::include_wgsl!("../shaders/lcd_column_average.wgsl");
+        let lcd_shader = gpu.device.create_shader_module(lcd_wgsl);
+
+        let pipeline_layout = gpu
+            .device
+            .create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+                label: Some("LcdFilter Column Averaging Pass Pipeline Layout"),
+                bind_group_layouts: &[&textures_bind_group_layout, &lcd_uniforms.bind_group_layout],
+                push_constant_ranges: &[],
+            });
+
+        let pipeline = gpu
+            .device
+            .create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+                label: Some("LcdFilter Column Averaging Pass Pipeline"),
+                layout: Some(&pipeline_layout),
+
+                vertex: wgpu::VertexState {
+                    module: &lcd_shader,
+                    entry_point: "vs_main",
+                    buffers: &[],
                 },
-            })],
-            depth_stencil_attachment: None,
-            timestamp_writes: None,
-            occlusion_query_set: None,
-        });
 
-        render_pass.set_pipeline(&self.display_pass_pipeline);
-        render_pass.set_bind_group(0, &self.display_pass_textures_bind_group, &[]);
-        render_pass.set_bind_group(1, &self.uniforms.bind_group, &[]);
-        render_pass.draw(0..3, 0..1); //FSQ
+                fragment: Some(wgpu::FragmentState {
+                    module: &lcd_shader,
+                    entry_point: "fs_main",
+                    targets: &[Some(wgpu::ColorTargetState {
+                        format: color_format,
+                        blend: Some(wgpu::BlendState {
+                            color: wgpu::BlendComponent::REPLACE,
+                            alpha: wgpu::BlendComponent::REPLACE,
+                        }),
+                        write_mask: wgpu::ColorWrites::ALL,
+                    })],
+                }),
 
-        self.frames_available_for_hysteresis = (self.frames_available_for_hysteresis + 1)
-            .min(gpu.color_attachment.layer_array_views.len());
+                primitive: wgpu::PrimitiveState {
+                    topology: wgpu::PrimitiveTopology::TriangleStrip,
+                    strip_index_format: None,
+                    front_face: wgpu::FrontFace::Cw,
+                    cull_mode: None,
+                    polygon_mode: wgpu::PolygonMode::Fill,
+                    unclipped_depth: false,
+                    conservative: false,
+                },
+
+                depth_stencil: None,
+
+                multisample: wgpu::MultisampleState {
+                    count: 1,
+                    mask: !0,
+                    alpha_to_coverage_enabled: false,
+                },
+
+                multiview: None,
+            });
+
+        (pipeline, textures_bind_group_layout, textures_bind_group)
     }
 }
